c68f67c7997fbd4f2b9371ef6a947039b5e96dfb
joaovitor05 <79288226+joaovitorp05@users.noreply.github.com>
2024-03-09 10:31:19 -0300

    Update 05-desafio.py

--- 05-desafio.py
+++ 05-desafio.py
@@ -2,6 +2,12 @@ segunda = int(input("voce trabalhou na segunda?:"))
 quarta = int(input("voce trabalhou na quarta?:"))
 sexta = int(input("voce trabalhou na sexta?:"))
 
+tvtubo = bool(segunda)
+print(f"João pode comprar uma tv de tubo? {tvtubo}")
+
+tv32 = bool(segunda) and bool(quarta)
+print(f"João pode comprar uma tv 32? {tv32}")
+
 tv55 = bool(segunda) and bool(quarta) and bool(sexta)
 print(f"João pode comprar uma tv 55? {tv55}")
 
