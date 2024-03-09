a2de717fce1e79ade52d37c276680e97b8ec58db
joaovitor05 <79288226+joaovitorp05@users.noreply.github.com>
2024-03-09 10:46:45 -0300

    Add files via upload

---
+++ 06-funcoesMatematicas.py
@@ -0,0 +1,29 @@
+import math
+
+x = 2
+y = 10
+
+print("Valor absoluto:")
+print(math.fabs(x))
+print("--------------------")
+print("Teto")
+print(math.ceil(y))
+print("--------------------")
+print("Piso")
+print(math.floor(y))
+print("------------------")
+print("Raiz quadrada")
+print(math.sqrt(x))
+print("-------------------")
+print("Parte inteira (truncamento)")
+print(math.trunc(y))
+print("-------------------")
+print("Fatorial")
+print(math.factorial(x))
+print("-------------------")
+print("Logaritmo")
+print(math.log(x, y))
+print("-------------------")
+print("PI")
+print(math.pi)
+print("-------------------")
