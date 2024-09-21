3c5e5c1f12d6c35c1ed115e2f157acb80468ecd9
joaovitor05 <79288226+joaovitorp05@users.noreply.github.com>
2024-09-20 21:01:30 -0300

    Add files via upload

---
+++ javaaula20.09/js/script.js
@@ -0,0 +1,25 @@
+//alert("ARQUIVO EXTERNO")
+/*document.write('MENSAGEM NA TELA')
+console.log('MENSAGEM NO CONSOLE')
+/*document = DOM
+ge0tElementById = método
+*/
+
+/*document.getElementById('nome').value='João'
+*/
+
+/*var texto = 'João'
+var numeroInteiro = 60
+var numeroFracionario = 1222.50
+var teste = true
+alert(texto)
+alert(numeroInteiro)
+alert(numeroFracionario)*/
+var nome = prompt("Digite seu nome: ")
+var idade = prompt("Digite sua idade: ")
+//var idade1 = 100
+alert('O nome é: '+nome+' e sua idade é:'+idade+' anos')
+aler
+
+//console.log(idade)
+//console.log(idade1)
